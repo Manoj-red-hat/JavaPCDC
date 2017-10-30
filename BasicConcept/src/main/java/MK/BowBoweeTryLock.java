ee88dd66c352d5ce67813456ddb90f03aa7c17f3
manoj.kumar1 <manoj.kumar.mbm@gmail.com>
2017-10-30 16:13:08 +0530

    Safe lock implementation

--- BasicConcept/src/main/java/MK/BowBoweeTryLock.java
+++ BasicConcept/src/main/java/MK/BowBoweeTryLock.java
@@ -1,4 +1,65 @@
 package MK;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class BowBoweeTryLock {
+    static class Friend {
+        String name;
+        private final Lock lock = new ReentrantLock();
+
+        Friend(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public boolean impendingBow(Friend bower) {
+            boolean myLock = false;
+            boolean yourLock = false;
+            try {
+                myLock = lock.tryLock();
+                yourLock = bower.lock.tryLock();
+            } finally {
+                if (!myLock && yourLock) {
+                    if (myLock) {
+                        lock.unlock();
+                    }
+                    if (yourLock) {
+                        bower.lock.unlock();
+                    }
+                }
+            }
+            return myLock && yourLock;
+        }
+
+        public void bow(Friend bower) {
+            if (impendingBow(bower)) {
+                try {
+                    System.out.format("%s: %s has"
+                                    + " bowed to me!%n",
+                            this.name, bower.getName());
+                    bower.bowBack(this);
+                } finally {
+                    lock.unlock();
+                    bower.lock.unlock();
+                }
+            } else {
+                System.out.format("%s: %s started"
+                                + " to bow to me, but saw that"
+                                + " I was already bowing to"
+                                + " him.%n",
+                        this.name, bower.getName());
+            }
+        }
+        public void bowBack(Friend bower) {
+            System.out.format("%s: %s has" +
+                            " bowed back to me!%n",
+                    this.name, bower.getName());
+        }
+    }
 }
+
+
